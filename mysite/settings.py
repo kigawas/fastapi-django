8a943999b797ad7f77cb0603082b2d4a48dcf2c7
Weiliang Li <to.be.impressive@gmail.com>
2020-10-15 21:59:26 +0900

    fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"  # noqa
     },
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
