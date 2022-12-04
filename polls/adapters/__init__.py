01af2f24184f7af68d463753836599b860b99e67
Weiliang Li <to.be.impressive@gmail.com>
2022-12-04 15:03:04 +0900

    Add async ORM queries (#88)

--- polls/adapters/__init__.py
+++ polls/adapters/__init__.py
@@ -8,26 +8,24 @@ from polls.models import Choice, Question
 ModelT = TypeVar("ModelT", bound=models.Model)
 
 
-def retrieve_object(model_class: Type[ModelT], id: int) -> ModelT:
-    instance = model_class.objects.filter(pk=id).first()
+async def retrieve_object(model_class: Type[ModelT], id: int) -> ModelT:
+    instance = await model_class.objects.filter(pk=id).afirst()
     if not instance:
         raise HTTPException(status_code=404, detail="Object not found.")
     return instance
 
 
-def retrieve_question(
-    q_id: int = Path(..., description="get question from db")
-) -> Question:
-    return retrieve_object(Question, q_id)
+async def retrieve_question(q_id: int = Path(..., description="get question from db")):
+    return await retrieve_object(Question, q_id)
 
 
-def retrieve_choice(c_id: int = Path(..., description="get choice from db")):
-    return retrieve_object(Choice, c_id)
+async def retrieve_choice(c_id: int = Path(..., description="get choice from db")):
+    return await retrieve_object(Choice, c_id)
 
 
-def retrieve_questions():
-    return Question.objects.all()
+async def retrieve_questions():
+    return [q async for q in Question.objects.all()]
 
 
-def retrieve_choices():
-    return Choice.objects.all()
+async def retrieve_choices():
+    return [c async for c in Choice.objects.all()]
