3cb4bb5284741c0ec14556565cd6ceb8a1dbbf7d
Weiliang Li <to.be.impressive@gmail.com>
2024-05-30 01:28:44 +0900

    Bump dependencies

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -6,6 +6,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/asgi/
 """
+
 import os
 
 from django.conf import settings
